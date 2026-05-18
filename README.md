c7c064bd9cecbdb8a81ae50f359917ffee1cb638
Jorge Montes <jomontes97@hotmail.com>
2026-05-18 18:37:28 -0500

    feat: Add basic Linux System Monitor script

---
+++ README.md
@@ -0,0 +1,23 @@
+# Linux System Monitor (Sistema de monitoreo de sistema Linux)
+
+Projecto practico de DevOps para monitorear recursos basicos de un sistema Linux utilizando Bash.
+
+## Objetivo
+
+Crear un script que nos pueda mostrar informacion basica del sistema, como fecha y hora, uso de CPU, uso de memoria, espacio en disco, y procesos con mayor consumo.
+
+## Tecnologias Utilizadas
+
+- Linux (Ubuntu)
+- Bash (Lenguaje de scripting)
+- NeoVim (Editor de texto)
+- Git (Control de versiones)
+- GitHub (Repositorio remoto)
+
+## Estructura del proyecto
+
+```text
+linux-system-monitor/
+├── README.md
+└── scripts/
+    └── system_monitor.sh
---
+++ scripts/system-monitor.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "==================================="
+echo " Linux System Monitor"
+echo "==================================="
+echo "Fecha y Hora"
+
+# Mostrar la fecha y hora actual
+date +"%Y-%m-%d %H:%M:%S"
+echo
+
+# Mostrar uso de Disco
+echo "Uso de disco"
+df -h
+echo
+
+# Uso de CPU
+echo "Uso de CPU"
+top -b -n 1 | grep "Cpu(s)"
+echo
+
+
+# Mostrar Memoria
+echo "Uso de memoria"
+free -h
+echo
+
+# Procesos de mayor consumo
+echo "Procesos de Mayor consumo"
+ps aux --sort=%mem | head -n 6
+echo
+
+# Mostrar mayor consumo de CPU
+echo "Consumo de CPU"
+ps aux --sort=%cpu | head -n 6
+echo
+
+echo "Monitoreo finalizado"
+
+
